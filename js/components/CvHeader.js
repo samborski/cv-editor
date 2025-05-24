5060d4045c314eaedf241060a5ceac1591d53adf
Daniel G. Samborski <danielgsamborski@gmail.com>
2025-05-24 12:37:18 -0300

    feat: mejorar visualización y manejo de la imagen de perfil en CvHeader, incluyendo ayuda contextual en modo edición

--- js/components/CvHeader.js
+++ js/components/CvHeader.js
@@ -1,6 +1,6 @@
 // js/components/CvHeader.js
 const CvHeader = {
-    // ... (props, emits, data, watch, methods sin cambios en su definición, solo en el template)
+    // ... (props, emits, data, watch, methods sin cambios) ...
     props: ['cvData', 'editMode'],
     emits: [],
     data() {
@@ -38,16 +38,28 @@ const CvHeader = {
     },
     template: `
     <header class="flex flex-col md:flex-row print:flex-row items-center bg-secondary dark:bg-dark-secondary p-5 rounded-lg mb-2 print:mb-4 print:p-2 print:!bg-white mt-16 print:mt-0">
+        <!-- Contenedor de la imagen de perfil y su input -->
         <div class="flex-shrink-0 mb-5 md:mb-0 print:mb-0 md:mr-5 print:mr-5">
-            <img :src="cvData.profilePicture" alt="Foto de perfil"
+            <!-- Mostrar la imagen solo si cvData.profilePicture no está vacío -->
+            <img v-if="cvData.profilePicture" 
+                 :src="cvData.profilePicture" 
+                 alt="Foto de perfil"
                  class="w-32 max-w-[150px] min-w-[80px] print:w-28 rounded-full object-cover border-2 border-primary print:!border-black shadow" />
+            
+            <!-- El input para la URL de la foto siempre se muestra en modo edición, 
+                 independientemente de si hay una foto o no, para permitir añadir una. -->
             <div v-if="editMode" class="relative mt-2">
                 <input type="text" v-model="cvData.profilePicture" placeholder="URL de tu foto de perfil" class="edit-input pr-7">
                 <i v-if="cvData.profilePicture" 
                    @click="clearField('profilePicture')" 
                    class="fa-solid fa-times-circle cursor-pointer text-gray-400 hover:text-red-500 absolute right-2 top-1/2 -translate-y-1/2"></i>
+                <!-- Texto de ayuda si no hay foto y estamos en modo edición -->
+                <p v-if="!cvData.profilePicture" class="text-xs text-gray-500 dark:text-gray-400 mt-1">
+                    Pega aquí la URL de una imagen para tu perfil.
+                </p>
             </div>
         </div>
+
         <div class="flex-1 text-center md:text-left print:text-left space-y-2">
             <h1 class="text-primary dark:text-dark-primary text-2xl md:text-3xl font-bold print:!text-black print:!mb-2">
                 <span v-if="!editMode">{{ cvData.name }}</span>
@@ -99,12 +111,10 @@ const CvHeader = {
                 <template v-else>
                     <span class="relative inline-block mr-1">
                         <input type="text" v-model="cvData.websiteDisplay" placeholder="Ej: Mi Portafolio" class="edit-input inline-block w-auto pr-7">
-                        <!-- Ayuda contextual para websiteDisplay -->
                         <span class="block text-xs text-gray-500 dark:text-gray-400 mt-1">Texto que se mostrará para el enlace.</span>
                     </span>
                     <span class="relative inline-block">
                         <input type="url" v-model="cvData.websiteUrl" placeholder="https://www.tusitio.com" class="edit-input inline-block w-auto pr-7">
-                         <!-- Ayuda contextual para websiteUrl -->
                         <span class="block text-xs text-gray-500 dark:text-gray-400 mt-1">URL completa de tu sitio web.</span>
                     </span>
                     <span v-if="websiteUrlError" class="text-red-500 text-xs ml-2 block">{{ websiteUrlError }}</span>
@@ -116,6 +126,7 @@ const CvHeader = {
                 <span v-if="!editMode">{{ cvData.address }}</span>
                 <span v-else class="relative block">
                     <input type="text" v-model="cvData.address" placeholder="Calle Número, Ciudad, Provincia" class="edit-input pr-7">
+                    <i v-if="cvData.address" @click="clearField('address')" class="fa-solid fa-times-circle cursor-pointer text-gray-400 hover:text-red-500 absolute right-2 top-1/2 -translate-y-1/2"></i>
                 </span>
             </p>
 
@@ -124,6 +135,7 @@ const CvHeader = {
                 <span v-if="!editMode">{{ cvData.birthdate }}</span>
                  <span v-else class="relative block">
                     <input type="text" v-model="cvData.birthdate" placeholder="DD de Mes de AAAA (ej: 18 de enero de 1973)" class="edit-input pr-7">
+                     <i v-if="cvData.birthdate" @click="clearField('birthdate')" class="fa-solid fa-times-circle cursor-pointer text-gray-400 hover:text-red-500 absolute right-2 top-1/2 -translate-y-1/2"></i>
                     <span class="block text-xs text-gray-500 dark:text-gray-400 mt-1">Este formato se usa para el VCF.</span>
                 </span>
             </p>
